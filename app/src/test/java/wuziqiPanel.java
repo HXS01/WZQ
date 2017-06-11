483c8b861ca44026cb3e6627861ede730ff3e465
acer-pc <1669118087@qq.com>
2017-06-11 20:57:11 +0800

    五子棋基本功能实现

---
+++ app/src/test/java/wuziqiPanel.java
@@ -0,0 +1,13 @@
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by acer-pc on 2017/6/8.
+ */
+
+public class wuziqiPanel extends View {
+    public wuziqiPanel(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
